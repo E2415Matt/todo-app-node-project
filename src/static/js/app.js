a1d0d2e31f1e6aff46834a66bea9cedd2555568c
root <root@ip-172-31-24-144.ec2.internal>
2020-11-10 20:41:25 +0000

    third time ammended the app.js file

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no tasks yet! Go ahead and add one!</p>
+                <p className="text-center">You haven't added any tasks yet! Go ahead and add one!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
